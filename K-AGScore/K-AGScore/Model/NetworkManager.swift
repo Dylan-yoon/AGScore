fe32f2800f44d28b45b7e1d6fb42d263da5fdc07
Dylan-Yoon <d.dylany21@gmail.com>
2023-10-05 21:52:50 +0900

    feat: Create ScoreData

---
+++ K-AGScore/K-AGScore/Model/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  K-AGScore
+//
+//  Created by Baem on 2023/10/05.
+//
+
+import Foundation
---
+++ K-AGScore/K-AGScore/Model/ScoreData.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreData.swift
+//  K-AGScore
+//
+//  Created by Baem on 2023/10/05.
+//
+
+struct ScoreData {
+    var rank: Int
+    var country: String
+    var gold: Int
+    var silver: Int
+    var bronze: Int
+    var medalTotal: Int
+    var rankMedalTotal: Int
+}
