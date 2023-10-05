aa3971f147322e05df47083f61fa21231502567e
Dylan-Yoon <d.dylany21@gmail.com>
2023-10-05 18:10:39 +0900

    build: 프로젝트 생성 및 Setting

---
+++ K-AGScore/K-AGScore/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  K-AGScore
+//
+//  Created by Baem on 2023/10/05.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        // Do any additional setup after loading the view.
+    }
+}
+
